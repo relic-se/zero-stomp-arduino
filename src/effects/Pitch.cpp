60b4cd907978b92ccdac42a37a51963551d2c41f
dcooperdalrymple <dcooperdalrymple@gmail.com>
2025-02-10 22:13:37 -0600

    Basic window pitch algorithm.

---
+++ src/effects/Pitch.cpp
@@ -0,0 +1,82 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Cooper Dalrymple
+//
+// SPDX-License-Identifier: GPLv3
+
+#include "effects/Pitch.h"
+
+Pitch::Pitch(size_t window, int16_t mix, size_t sample_rate, uint8_t channels) : Effect(mix, channels) {
+    setSampleRate(sample_rate);
+    setWindow(window);
+    reset();
+};
+
+void Pitch::setShift(float value) {
+    _shift = value;
+    updateRate();
+};
+
+void Pitch::setWindow(size_t value) {
+    _window = max(value, 2);
+    updateRate();
+    if (_buffer) reset();
+};
+
+void Pitch::setSampleRate(size_t value) {
+    _sample_rate = value;
+    updateRate();
+    if (_buffer) reset();
+};
+
+void Pitch::setChannels(uint8_t value) {
+    Effect::setChannels(value);
+    if (_buffer) reset();
+};
+
+void Pitch::process(int32_t *l, int32_t *r) {
+    *l = processChannel(*l, 0);
+    if (_isStereo) {
+        *r = processChannel(*r, 1);
+    }
+    
+    if (++_write >= _window) _write = 0;
+
+    _read += _rate;
+    if (_read >= _window << PITCH_SHIFT) _read -= _window << PITCH_SHIFT;
+};
+
+int32_t Pitch::processChannel(int32_t sample, uint8_t channel) {
+    // Update current position of buffer
+    _buffer[_write + _window * channel] = (sample_t)clip(sample);
+
+    size_t index = _read >> PITCH_SHIFT;
+    int32_t output = _buffer[index + _window * channel];
+
+    #if PITCH_SHIFT == 8
+    // Blend current sample with next sample
+    int16_t pos = _read & ((1 << PITCH_SHIFT) - 1);
+    if (pos) {
+        output = applyMix<int8_t>(
+            output,
+            _buffer[((index + 1) % _window) + _window * channel],
+            (int8_t)pos - (1 << (PITCH_SHIFT - 1))
+        );
+    }
+    #endif
+
+    // Mix with dry signal and return
+    return applyMix<int16_t>(sample, output, _mix);
+};
+
+void Pitch::updateRate() {
+    _rate = (size_t)(pow(2, _shift / 12.0) * (1 << PITCH_SHIFT));
+};
+
+void Pitch::reset() {
+    if (_buffer) {
+        free(_buffer);
+        _buffer = nullptr;
+    }
+    _buffer = (sample_t *)malloc(_window * (_isStereo + 1) * sizeof(sample_t));
+    memset((void *)_buffer, 0, _window * (_isStereo + 1) * sizeof(sample_t));
+    _write = _read = 0;
+};
---
+++ src/effects/Pitch.h
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: Copyright (c) 2025 Cooper Dalrymple
+//
+// SPDX-License-Identifier: GPLv3
+
+#ifndef _ZERO_CHORUS_H
+#define _ZERO_CHORUS_H
+
+#include "Arduino.h"
+#include "ZeroStomp.h"
+#include "effects/Effect.h"
+
+#define PITCH_DEFAULT_WINDOW (DEFAULT_SAMPLE_RATE * 0.05) // 50ms
+#define PITCH_SHIFT (8)
+
+class Pitch : public Effect
+{
+
+public:
+    Pitch(size_t window = PITCH_DEFAULT_WINDOW, int16_t mix = MAX_VALUE(int16_t), size_t sample_rate = DEFAULT_SAMPLE_RATE, uint8_t channels = DEFAULT_CHANNELS);
+
+    void setShift(float value); // semitones
+    void setWindow(size_t value);
+    void setSampleRate(size_t value);
+    void setChannels(uint8_t value) override;
+
+    void process(int32_t *l, int32_t *r = nullptr);
+
+protected:
+    int32_t processChannel(int32_t sample, uint8_t channel);
+    void updateRate();
+    void reset();
+
+private:
+    float _shift;
+    size_t _window, _overlap, _sample_rate;
+    size_t _write;
+    size_t _read, _rate; // << PITCH_SHIFT
+    sample_t *_buffer;
+
+};
+
+#endif
